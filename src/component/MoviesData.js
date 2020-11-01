edea66a006486c43365e055c247398bc740ba406
omar-gmc <omar.b.brahim@gmail.com>
2020-11-01 22:08:48 +0100

    first commit

---
+++ src/component/MoviesData.js
@@ -0,0 +1,26 @@
+export const moviesData = [
+  {
+    id:Math.random(),
+    name:"contriatempo",
+    image:"https://fr.web.img6.acsta.net/c_215_290/pictures/16/11/18/11/16/356441.jpg",
+    rating:4,
+    year:2016,
+    
+  },
+  {
+    id:Math.random(),
+    name:"joker",
+    image:"https://fr.web.img6.acsta.net/pictures/19/09/03/12/02/4765874.jpg",
+    rating:4,
+    year:2019
+  },
+  {
+    id:Math.random(),
+    name:"titanic",
+    image:"https://media.senscritique.com/media/000004715818/source_big/Titanic.jpg" ,
+    rating:5,
+    year:1997 
+  }
+    
+  ];
+  
